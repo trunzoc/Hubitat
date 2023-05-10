9d00573385f9e8e8b387fdfc34a1fbb3b894cff7
trunzoc <trunzoc@users.noreply.github.com>
2023-05-10 13:04:05 -0400

    Update Anycubic_Photon_MonoX_Poller.groovy

--- Drivers/Anycubic_Photon_MonoX_Poller/Anycubic_Photon_MonoX_Poller.groovy
+++ Drivers/Anycubic_Photon_MonoX_Poller/Anycubic_Photon_MonoX_Poller.groovy
@@ -6,7 +6,7 @@
 *   Modification History:
 *       Date       Who                   What
 *       2023-05-01 Craig Trunzo          Built it
-*       2023-05-09 Craig Trunzo          Added "sysinfo
+*       2023-05-09 Craig Trunzo          Added "sysinfo"
 *
 *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 *  in compliance with the License. You may obtain a copy of the License at:
